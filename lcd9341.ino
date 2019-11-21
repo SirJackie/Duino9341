56bd5020b7be2772863e3b651a9c2268e592dacf
lyj00912 <3122742760@qq.com>
2019-11-21 13:45:12 +0800

    update the demo and find out the real problem in LCD_Fill()

--- lcd9341.ino
+++ lcd9341.ino
@@ -152,11 +152,44 @@ inline unsigned int RGB(unsigned short r,unsigned short g,unsigned short b){
 
 void setup(){
   Lcd_Init();
+  //Clear the screen
+  LCD_Fill(0,0,240,320,RGB(0,0,0));
   LCD_Fill(0,0,240,320,RGB(31,63,31));
-  for(int i = 0;i<32;i++){
-    LCD_Fill(10+i,10+i,220-(2*i),300-(2*i),RGB(31-i,(31-i)*2,31-i));
+  //Write 240 Red Pixel
+  digitalWrite(LCD_CS,LOW);//StartWriting
+  Address_set(0,0,239,1000000);
+  for(int i = 0;i<240;i++){
+    Lcd_Write_Data(RGB(255,0,0)>>8);
+    Lcd_Write_Data(RGB(255,0,0));
   }
-  LCD_Fill(10+32,10+32,220-(2*32),300-(2*32),RGB(31,63,31));
+  digitalWrite(LCD_CS,HIGH);//EndWriting
+  delay(2000);
+  
+  //Clear the screen
+  LCD_Fill(0,0,240,320,RGB(0,0,0));
+  LCD_Fill(0,0,240,320,RGB(31,63,31));
+  //Write 241 Red Pixel
+  digitalWrite(LCD_CS,LOW);//StartWriting
+  Address_set(0,0,239,1000000);
+  for(int i = 0;i<=240;i++){
+    Lcd_Write_Data(RGB(255,0,0)>>8);
+    Lcd_Write_Data(RGB(255,0,0));
+  }
+  digitalWrite(LCD_CS,HIGH);//EndWriting
+  delay(2000);
+
+  //Clear the screen
+  LCD_Fill(0,0,240,320,RGB(0,0,0));
+  LCD_Fill(0,0,240,320,RGB(31,63,31));
+  //Write 241 Red Pixel with Address_set(0,0,240,1000000);
+  digitalWrite(LCD_CS,LOW);//StartWriting
+  Address_set(0,0,240,1000000);
+  for(int i = 0;i<=240;i++){
+    Lcd_Write_Data(RGB(255,0,0)>>8);
+    Lcd_Write_Data(RGB(255,0,0));
+  }
+  digitalWrite(LCD_CS,HIGH);//EndWriting
+  delay(2000);
 }
 
 void loop(){
