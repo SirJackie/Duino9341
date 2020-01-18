8d338a572c597e11cb6e3d857a842888fcedc17e
lyj00912 <3122742760@qq.com>
2020-01-18 13:40:22 +0800

    create a colorful line draw demo

--- Duino9341.ino
+++ Duino9341.ino
@@ -185,8 +185,7 @@ void LcdDrawLine(int x1, int y1, int x2, int y2, unsigned int color){
   }
 }
 
-int x = 0;
-int y = 0;
+int x1 = 120,y1 = 160,x2 = 239,y2 = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -194,8 +193,24 @@ void setup(){
   LcdFill(0,0,239,319,RGB(255,255,255));
 }
 
+int color = RGB(0,0,0)-1000;
+
 void loop(){
-  LcdDrawLine(0,0,239,319,RGB(0,0,0));
-  delay(20);
-  LcdDrawLine(0,0,239,319,RGB(255,255,255));
+  LcdDrawLine(x1,y1,x2,y2,color);
+  if(x2 == 239 && y2 == 0){
+    color += 1000;
+    y2 += 1;
+  }
+  else if(y2 >= 0 && y2 <  319 && x2 == 239){
+    y2 += 1;
+  }
+  else if(x2 >  0 && x2 <= 239 && y2 == 319){
+    x2-= 1;
+  }
+  else if(y2 >  0 && y2 <= 319 && x2 == 0){
+    y2 -= 1; 
+  }
+  else if(x2 >= 0 && x2 <  239 && y2 == 0){
+    x2 += 1;
+  }
 }
